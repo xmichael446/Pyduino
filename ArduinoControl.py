6c60257ac4516c34840bf0b8f1748124ea542c5f
Muhammadrasul <muhammadrasul446@mail.ru>
2020-05-09 05:21:05 +0500

    Bug fixes, compatibility with all types of serial ports

--- ArduinoControl.py
+++ ArduinoControl.py
@@ -10,18 +10,30 @@ theme = 'DarkBlack1'
 # set the theme:
 sg.theme(theme)
 
+# Your Operating System. You can skip this, coz it's just for determining corect serial port
+
+OS = "Unix"
+port_annot = " "
+
+if OS == "Unix":
+	port_annot = "/dev/ttyACM0"
+elif OS == "Windows":
+	port_annot = "COM"
+else:
+	port_annot = "/dev/tty.usbserial"
+
 # 12 ports to be contolled:
 digital_ports = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
 # Window elements' position, type and values, where needed:
 window_layout = [  
 		[sg.T('Arduino Control using Python!!!')],
-		[sg.T("Serial Port"), sg.In("COM", size=(20, 1)), sg.Button("Connect")],
+		[sg.T("Serial Port"), sg.In(port_annot, size=(20, 1), key='port'), sg.Button("Connect")],
 		[sg.CBox("Port 2", size=(8, 1)), sg.CBox("Port 3", size=(8, 1)), sg.CBox("Port 4", size=(8, 1))],
 		[sg.CBox("Port 5", size=(8, 1)), sg.CBox("Port 6", size=(8, 1)), sg.CBox("Port 7", size=(8, 1))],
 		[sg.CBox("Port 8", size=(8, 1)), sg.CBox("Port 9", size=(8, 1)), sg.CBox("Port 10", size=(8, 1))],
 		[sg.CBox("Port 11", size=(8, 1)), sg.CBox("Port 12", size=(8, 1)), sg.CBox("Port 13", size=(8, 1))],
-		[sg.Button("Send"), sg.Button("Exit")]
+		[sg.Button("Send"), sg.Button("Exit"), sg.T(" ", size=(24, 1), key="error", text_color="red")]
 ]
 
 # create a window object:
@@ -36,14 +48,16 @@ while True:
 
 	# setup the serial port:
 	elif event == "Connect":
-		board = pyfirmata.Arduino(values[0])
+
+		board = pf.Arduino(values["port"])
 
 	# change states of pins, according to checkboxes:
 	elif event == "Send":
-
 		for i in digital_ports:
-			for j in values:
-				board.digital[i].write(values[j + 1])
+			j = 0
+			while j < 12:
+				board.digital[i].write(values[j])
+				j += 1
 
 
 window.close() # when done, close.
