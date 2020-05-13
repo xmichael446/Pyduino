55c5e220971735d13160a71174da662b2aca7df1
Muhammadrasul <muhammadrasul446@mail.ru>
2020-05-14 01:44:07 +0500

    Now working version

--- Pyduino.py
+++ Pyduino.py
@@ -14,40 +14,8 @@ sg.theme(theme)
 # Boolean to check connection
 connected = False
 
-# 12 ports to be contolled:
-digital_ports = {2:0, 4:8, 7:9, 8:2, 12:7, 13:11}
-pwm_ports = {3:4, 5:1, 6:5, 9:6, 10:10, 11:3}
-
-board_get_pwm = dict()
-
-# Window elements' position, type and values, where needed:
-column_one = [
-
-		[sg.CB("Port 2", size=(8, 1))],
-
-		[sg.T("Port ~5", size=(8, 1)), sg.Spin([i for i in range(0, 51)], size=(2, 2))],
-
-		[sg.CB("Port 8", size=(8, 1))],
-
-		[sg.T("Port ~11", size=(8, 1)), sg.Spin([i for i in range(0, 51)], size=(2, 2))]
-
-]
-column_two = [
-		[sg.T("Port ~3", size=(8, 1)), sg.Spin([i for i in range(0, 51)], size=(2, 2))],
-		[sg.T("Port ~6", size=(8, 1)), sg.Spin([i for i in range(0, 51)], size=(2, 2))],
-		[sg.T("Port ~9", size=(8, 1)), sg.Spin([i for i in range(0, 51)], size=(2, 2))],
-
-		[sg.CB("Port 12", size=(8, 1))]
-
-]
-column_three = [
-		[sg.CB("Port 4", size=(8, 1))],
-		[sg.CB("Port 7", size=(8, 1))],
-
-		[sg.T("Port ~10", size=(8, 1)), sg.Spin([i for i in range(0, 51)], size=(2, 2))],
-
-		[sg.CB("Port 13", size=(8, 1))]
-]
+board_type = " "
+digital_ports, board_get_pwm, pwm_ports = dict(), dict(), dict()
 
 gitoverflow = [
 		[sg.T("Github: Muhammadrasul446", size=(28, 1))],
@@ -59,81 +27,168 @@ twittedin = [
 		[sg.T("Linkedin: 6644821a9", size=(28, 1))]
 ]
 
-window_layout = [
+# Layout if board is arduino nano
+nano_col_1 = [
+		[sg.CB("Port 2", size=(8, 1))],
+		[sg.T("Port ~3", size=(8, 1)), sg.Spin([i for i in range(0, 51)], size=(2, 2))],
+		[sg.CB("Port 4", size=(8, 1))],
+		[sg.T("Port ~5", size=(8, 1)), sg.Spin([i for i in range(0, 51)], size=(2, 2))],
+]
+
+nano_col_2 = [
+		[sg.T("Port ~6", size=(8, 1)), sg.Spin([i for i in range(0, 51)], size=(2, 2))],
+		[sg.CB("Port 7", size=(8, 1))],
+		[sg.CB("Port 8", size=(8, 1))],
+		[sg.T("Port ~9", size=(8, 1)), sg.Spin([i for i in range(0, 51)], size=(2, 2))],
+]
+
+nano_col_3 = [
+		[sg.T("Port ~10", size=(8, 1)), sg.Spin([i for i in range(0, 51)], size=(2, 2))],
+		[sg.T("Port ~11", size=(8, 1)), sg.Spin([i for i in range(0, 51)], size=(2, 2))],
+		[sg.CB("Port 12", size=(8, 1))],
+		[sg.CB("Port 13", size=(8, 1))],
+]
+
+nano_win_layout = [
 
 		[sg.T('Made by Muhammadrasul Abdulhayev')],
 
 		[sg.T("Serial Port"), sg.In("/dev/ttyACM0", size=(20, 1), key='port'), sg.Button("Connect")],
-		[sg.Column(column_one),  sg.VerticalSeparator(pad=None), sg.Column(column_two),  sg.VerticalSeparator(pad=None), sg.Column(column_three)],
+		[sg.Column(nano_col_1),  sg.VerticalSeparator(pad=None), sg.Column(nano_col_1),  sg.VerticalSeparator(pad=None), sg.Column(nano_col_1)],
 		[sg.Button("Send"), sg.Button("Exit")],
 
 		[sg.Column(gitoverflow),  sg.VerticalSeparator(pad=None), sg.Column(twittedin)]
 
 ]
 
-# create a window object:
-window = sg.Window('Pyduino', window_layout) 
 
-# keep the window open:
+# Layout if board is arduino mega
+mega_col_1 = [
+		[sg.T('Port ~' + str(i), size=(8, 1)), sg.Spin([j for j in range(0, 51)], size=(2, 2))] for i in range(2, 14)
+]
+mega_col_1.append([sg.CB("Port 14", size=(8, 1))])
+
+
+mega_col_2 = [
+		[sg.CB('Port ' + str(i), size=(8, 1))] for i in range(15, 28)
+]
+
+mega_col_3 = [
+		[sg.CB('Port ' + str(i), size=(8, 1))] for i in range(28, 41)
+]
+mega_col_4 = [
+		[sg.CB('Port ' + str(i), size=(8, 1))] for i in range(41, 54)
+]
+
+mega_win_layout = [
+
+		[sg.T('Made by Muhammadrasul Abdulhayev')],
+
+		[sg.T("Serial Port"), sg.In("/dev/ttyACM0", size=(20, 1), key='port'), sg.Button("Connect")],
+		[sg.Column(mega_col_1), sg.VerticalSeparator(pad=None), sg.Column(mega_col_2), sg.VerticalSeparator(pad=None), sg.Column(mega_col_3), sg.VerticalSeparator(pad=None), sg.Column(mega_col_4)],
+		[sg.Button("Send"), sg.Button("Exit")],
+
+		[sg.Column(gitoverflow),  sg.VerticalSeparator(pad=None), sg.Column(twittedin)]
+
+]
+
+
+select_win_lay = [
+			[sg.T('Select your board:')],
+			[sg.Button("Arduino Nano/Uno/Leonardo")],
+			[sg.Button("Arduino Mega/Mega2560")]
+]
+
+select_win = sg.window("Select your board", select_win_lay)
+
 while True:
-
-	event, values = window()
-
-	# untill it's closed manually:
-	if event in (None, 'Exit'):
+	event = select_win()
+	if event == None:
 		break
+	elif event == "Arduino Nano/Uno/Leonardo":
+		board_type = "nano"
+	elif event == "Arduino Mega/Mega2560":
+		board_type = "mega"
+select_win.close(); del select_win, select_win_lay
 
-	# setup the serial port:
-	elif event == "Connect":
+if board == "nano":
+	digital_ports = {2:0, 4:2, 7:5, 8:6, 12:10, 13:11}
+	pwm_ports = {3:1, 5:3, 6:4, 9:7, 10:8, 11:9}
 
-		if not connected:
+	del mega_win_layout, mega_col_1, mega_col_2, mega_col_3, mega_col_4
+	window = sg.Window('Pyduino', nano_win_layout)
 
-			try:
+elif board == "mega":
+	digital_ports = dict(zip([i for i in range(14, 54)], [i for i in range(12, 52)]))
+	pwm_ports = dict(zip([i for i in range(2, 14)], [i for i in range(0, 13)]))
 
-				# define the 'board'-object
-				board = pf.Arduino(values["port"])
-				sg.popup_ok("Connected")
+	del nano_win_layout, nano_col_1, nano_col_2, nano_col_3
+	window = sg.Window('Pyduino', mega_win_layout)
 
-				for i in pwm_ports.keys():
+def run():
+	# keep the window open:
+	while True:
 
-					# Get pwm pins of the board and fill the dictionary:
-					board_get_pwm[i] = board.get_pin("d:" + str(i) + ":p")
+		event, values = window()
 
-				connected = True
+		# untill it's closed manually:
+		if event in (None, 'Exit'):
+			break
 
-			# if serial port is not defined or empty
-			except:
+		# setup the serial port:
+		elif event == "Connect":
 
-				# let the user know
-				sg.popup_error("Serial port is invalid!!!", text_color="red")
+			if not connected:
 
-				connected = False
+				try:
 
-		else:
-			# If already connected:
-			sg.popup_ok("Already connected")
+					# define the 'board'-object
+					if board_type == "nano":
+						board = pf.Arduino(values["port"])
+					else:
+						board = pf.ArduinoMega(values["port"])
+					sg.popup_ok("Connected")
 
-	# change states of pins, according to checkboxes:
-	elif event == "Send":
+					for i in pwm_ports.keys():
 
-		if connected:
+						# Get pwm pins of the board and fill the dictionary:
+						board_get_pwm[i] = board.get_pin("d:" + str(i) + ":p")
 
-			# digital pins:
-			for i in digital_ports.keys():
+					connected = True
 
-				state = values[digital_ports[i]]
-				board.digital[i].write(state)
+				# if serial port is not defined or empty
+				except:
 
-			# pwm pins:
-			for j in pwm_ports.keys():
+					# let the user know
+					sg.popup_error("Serial port is invalid!!!", text_color="red")
 
-				state = float((values[pwm_ports[j]])/50)
-				board_get_pwm[j].write(state)
+					connected = False
 
-		else:
+			else:
+				# If already connected:
+				sg.popup_ok("Already connected")
 
-			# If not connected
-			sg.popup_error("Connect the serial port first!", text_color="red")
+		# change states of pins, according to checkboxes:
+		elif event == "Send":
+
+			if connected:
+
+				# digital pins:
+				for i in digital_ports.keys():
+
+					state = values[digital_ports[i]]
+					board.digital[i].write(state)
+
+				# pwm pins:
+				for j in pwm_ports.keys():
+
+					state = float((values[pwm_ports[j]])/50)
+					board_get_pwm[j].write(state)
+
+			else:
+
+				# If not connected
+				sg.popup_error("Connect the serial port first!", text_color="red")
 
 
-window.close(); del window # when done, close.
+	window.close(); del window # when done, close.
